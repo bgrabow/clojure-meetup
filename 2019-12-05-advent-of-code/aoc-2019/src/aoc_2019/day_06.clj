03b1679d2fefaf6e111fc6981058c94623a8bd9b
Ben Grabow <ben.grabow@singlewire.com>
2019-12-06 11:08:07 -0600

    Defer counting of parents until final step of p1

--- 2019-12-05-advent-of-code/aoc-2019/src/aoc_2019/day_06.clj
+++ 2019-12-05-advent-of-code/aoc-2019/src/aoc_2019/day_06.clj
@@ -16,22 +16,9 @@
 (def orbits
   (parse-orbits (slurp "resources/input-06.txt")))
 
-(defn parent-orbits
+(defn parent-bodies
   [orbits body]
   (take-while some? (rest (iterate orbits body))))
-
-(defn orbit-depth
-  [orbits body]
-  (count (parent-orbits orbits body)))
-
-(defn common-root-depth
-  [orbits a b]
-  (apply max (map (partial orbit-depth orbits)
-                  (set/intersection (set (parent-orbits orbits a))
-                                    (set (parent-orbits orbits b))))))
-
-(def test-orbits
-  (parse-orbits "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN"))
 
 (defn symmetric-difference
   [s1 s2]
@@ -40,15 +27,13 @@
 
 (defn solve-p1
   [orbits]
-  (reduce +
-          (map (partial orbit-depth orbits)
-               (keys orbits))))
+  (count (mapcat (partial parent-bodies orbits) (keys orbits))))
 
 (defn solve-p2
   [orbits]
   (count
-    (symmetric-difference (set (parent-orbits orbits "YOU"))
-                          (set (parent-orbits orbits "SAN")))))
+    (symmetric-difference (set (parent-bodies orbits "YOU"))
+                          (set (parent-bodies orbits "SAN")))))
 
 (comment
   (time (solve-p1 orbits))
