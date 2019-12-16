d15e2bd500b35c00688869d0c24077f06f2837f2
Ben Grabow <ben.grabow@singlewire.com>
2019-12-16 16:02:44 -0600

    Solve d16

--- 2019-12-05-advent-of-code/aoc-2019/project.clj
+++ 2019-12-05-advent-of-code/aoc-2019/project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/math.combinatorics "0.1.6"]
-                 [org.clojure/core.async "0.5.527"]])
+                 [org.clojure/core.async "0.5.527"]
+                 [hashp "0.1.0"]]
+  :injections [(require 'hashp.core)])
