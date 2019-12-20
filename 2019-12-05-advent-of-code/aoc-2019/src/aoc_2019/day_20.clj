208064ac3b9c5a83040e6eb908720b5e1fd1e4fd
Ben Grabow <ben.grabow@singlewire.com>
2019-12-20 11:24:48 -0600

    Solve d20p2

--- 2019-12-05-advent-of-code/aoc-2019/src/aoc_2019/day_20.clj
+++ 2019-12-05-advent-of-code/aoc-2019/src/aoc_2019/day_20.clj
@@ -35,17 +35,42 @@
                               (neighbors p)))]
         [entrance (sort [id-1 id-2])]))))
 
+(def center
+  [(/ (apply max (map first (keys maze))) 2)
+   (/ (apply max (map second (keys maze))) 2)])
+
+(defn signum
+  [x]
+  (if (zero? x)
+    0
+    (/ x (Math/abs ^long x))))
+
+(defn inside-ring?
+  [p]
+  (let [test-point (->> (map - center p)
+                        (map signum)
+                        (map #(* % 3))
+                        (mapv + p))]
+    (= \space (maze test-point))))
+
 (defn format-portal
   [[[p1] [p2]]]
   (when (and p1 p2)
-    [p1 p2]))
+    (vec (sort-by inside-ring? [p1 p2]))))
 
-(def portals (->> (keep (partial portal maze) (keys maze))
-                  (group-by second)
-                  (map second)
-                  (map format-portal)
-                  (into {})
-                  (#(merge % (set/map-invert %)))))
+(def portals {:outward (->> (keep (partial portal maze) (keys maze))
+                            (group-by second)
+                            (map second)
+                            (map format-portal)
+                            (into {}))
+              :inward (set/map-invert
+                        (->> (keep (partial portal maze) (keys maze))
+                             (group-by second)
+                             (map second)
+                             (map format-portal)
+                             (into {})))})
+
+(def flat-portals (merge (:inward portals) (:outward portals)))
 
 (def start (-> (keep (partial portal maze) (keys maze))
                (set/map-invert)
@@ -57,7 +82,7 @@
 
 (defn non-euclidean-neighbors
   [location]
-  (->> (conj (neighbors location) (portals location))
+  (->> (conj (neighbors location) (flat-portals location))
        (filter (comp #{\.} maze))))
 
 (defn step-search
@@ -77,3 +102,42 @@
        (drop-while #(not ((:visited %) end)))
        first
        :distance))
+
+(defn recursive-neighbors
+  [p]
+  (let [inward-neighbor (some-> ((:inward portals) (subvec p 0 2))
+                                (conj (inc (p 2))))
+        outward-neighbor (when
+                           (pos? (p 2))
+                           (some-> ((:outward portals) (subvec p 0 2))
+                                   (conj (dec (p 2)))))
+        flat-neighbors (->> (neighbors (subvec p 0 2))
+                            (filter (comp #{\.} maze))
+                            (map #(conj % (p 2))))]
+    (remove nil? (conj flat-neighbors inward-neighbor outward-neighbor))))
+
+(defn step-recursive-search
+  [{:keys [frontier visited distance]}]
+  (let [new-frontier (->> (mapcat recursive-neighbors frontier)
+                          (remove visited)
+                          set)]
+    {:frontier new-frontier
+     :visited (into visited new-frontier)
+     :distance (inc distance)}))
+
+(defn solve-p2
+  []
+  (->> (iterate step-recursive-search {:frontier #{(conj start 0)}
+                                       :visited #{(conj start 0)}
+                                       :distance 0})
+       (drop-while #(not ((:visited %) (conj end 0))))
+       first
+       :distance))
+
+(comment
+  (recursive-neighbors [59 92 0]))
+
+(comment
+  (->> (iterate step-recursive-search {:frontier #{(conj start 0)}
+                                       :visited #{(conj start 0)}
+                                       :distance 0})))
